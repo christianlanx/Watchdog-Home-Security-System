d69a3de31f657ea4262c5c4b51e86dbe15991c05
unknown <morgan.mccandless1@yahoo.com>
2021-04-27 11:10:34 -0700

    Made sure the clock period was actually right -- that's important

--- sensorHub/timerTutorial/Core/Src/main.c
+++ sensorHub/timerTutorial/Core/Src/main.c
@@ -338,7 +338,7 @@ static void MX_TIM2_Init(void)
   htim2.Instance = TIM2;
   htim2.Init.Prescaler = 0;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 4294967295;
+  htim2.Init.Period = 167999999;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
