0bb8bf685f97d4d6537369c2694dadef770eaf68
unknown <morgan.mccandless1@yahoo.com>
2021-04-28 18:10:34 -0700

    Fixed clocking stuff:

--- sensorHub/timerTutorial/Core/Src/main.c
+++ sensorHub/timerTutorial/Core/Src/main.c
@@ -340,9 +340,9 @@ static void MX_TIM2_Init(void)
 
   /* USER CODE END TIM2_Init 1 */
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 0;
+  htim2.Init.Prescaler = 42000-1;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 167999999;
+  htim2.Init.Period = 2000-1;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
