2d288bee2426986c3f270a51dc95031d9fb0df7b
Morgan McCandless <user@Users-MacBook-Pro.local>
2021-04-28 17:12:52 -0700

    fixing merging issues

--- sensorHub/timerTutorial/Core/Src/main.c
+++ sensorHub/timerTutorial/Core/Src/main.c
@@ -117,11 +117,12 @@ int main(void)
     HAL_TIM_Base_Start(&htim2);
   while (1)
   {
+      MX_USB_HOST_Process();
       HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
       HAL_Delay(500);
       HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_SET);
       /* USER CODE END WHILE */
-    MX_USB_HOST_Process();
+    
 
     /* USER CODE BEGIN 3 */
   }
