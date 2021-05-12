d0c9472f9630f7a8a7fe50dbdeb4be3556e3fee3
unknown <morgan.mccandless1@yahoo.com>
2021-05-12 16:49:21 -0700

    Updated settings for sound sensor

--- sensorHub/sound_sensor/Core/Src/stm32f4xx_it.c
+++ sensorHub/sound_sensor/Core/Src/stm32f4xx_it.c
@@ -57,7 +57,8 @@
 
 /* External variables --------------------------------------------------------*/
 extern HCD_HandleTypeDef hhcd_USB_OTG_FS;
-extern TIM_HandleTypeDef htim2;
+extern ADC_HandleTypeDef hadc1;
+extern ADC_HandleTypeDef hadc2;
 /* USER CODE BEGIN EV */
 
 /* USER CODE END EV */
@@ -215,17 +216,18 @@ void EXTI0_IRQHandler(void)
 }
 
 /**
-  * @brief This function handles TIM2 global interrupt.
+  * @brief This function handles ADC1, ADC2 and ADC3 global interrupts.
   */
-void TIM2_IRQHandler(void)
+void ADC_IRQHandler(void)
 {
-  /* USER CODE BEGIN TIM2_IRQn 0 */
+  /* USER CODE BEGIN ADC_IRQn 0 */
 
-  /* USER CODE END TIM2_IRQn 0 */
-  HAL_TIM_IRQHandler(&htim2);
-  /* USER CODE BEGIN TIM2_IRQn 1 */
+  /* USER CODE END ADC_IRQn 0 */
+  HAL_ADC_IRQHandler(&hadc1);
+  HAL_ADC_IRQHandler(&hadc2);
+  /* USER CODE BEGIN ADC_IRQn 1 */
 
-  /* USER CODE END TIM2_IRQn 1 */
+  /* USER CODE END ADC_IRQn 1 */
 }
 
 /**
